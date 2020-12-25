cd716bf7a737187651fc7b2e147590e719ed710c
ramiro6 <ramiroruiz578@gmail.com>
2020-12-25 16:15:18 +0100

    update version ui 11.0.5 cli

--- projects/ngx-iam-ui/src/lib/ngx-iam-ui.module.ts
+++ projects/ngx-iam-ui/src/lib/ngx-iam-ui.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { NgxIamUiComponent } from './ngx-iam-ui.component';
-import { WelcomeComponent } from './module/welcome/pages/welcome/welcome.component';
 
 
 
 @NgModule({
-  declarations: [NgxIamUiComponent, WelcomeComponent],
-  imports: [
-  ],
+  declarations: [NgxIamUiComponent],
+  imports: [],
   exports: [NgxIamUiComponent]
 })
 export class NgxIamUiModule { }
