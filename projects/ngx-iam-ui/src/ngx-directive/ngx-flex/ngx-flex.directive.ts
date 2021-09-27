da855f1932d279c2df02c4ec9156ce242aa8ee2b
ramiro6 <ramiroruiz578@gmail.com>
2021-09-27 19:56:01 +0200

    add directive base

--- projects/ngx-iam-ui/src/ngx-directive/ngx-flex/ngx-flex.directive.ts
+++ projects/ngx-iam-ui/src/ngx-directive/ngx-flex/ngx-flex.directive.ts
@@ -1,11 +1,10 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { NgxDirectiveBase } from '../ngx-directive-base';
-
-// const inputs = ['NgxFlex'];
 
 @Directive({ selector: '[NgxFlex]', inputs: ['options: NgxFlex'] })
 export class NgxFlexDirective extends NgxDirectiveBase {
   protected options: string;
+  protected DIRECTIVE_KEY: string = 'flex';
 
   constructor(el: ElementRef, _render: Renderer2) {
     super(el, _render);
